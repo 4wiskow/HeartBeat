feb03f2d9a6aea83d8655614303deff1d96fff7c
6steep <6steep@informatik.uni-hamburg.de>
2020-02-01 02:00:53 +0100

    added Segment and player movement

---
+++ Assets/Scripts/MoveSegment.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSegment : MonoBehaviour
+{
+    public float speed = 3f;
+    public GameObject segment;
+    private List<GameObject> segments = new List<GameObject>();
+    public int nSegments = 50;
+    public int killzoneBehindCamera = 100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float segmentSize = segment.transform.localScale.z;
+        for(int i = 0; i<50; i++)
+        {
+            segments.Add(Instantiate(segment, new Vector3(0, 0, i * segmentSize * 10), Quaternion.identity));
+        
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveAllSegments();
+    }
+
+    private void moveAllSegments()
+    {
+        GameObject toDestroy = null;
+        foreach (GameObject segment in segments)
+        {
+            if ((segment.transform.position.z + speed) >= Camera.main.transform.position.z - killzoneBehindCamera)
+            {
+
+                segment.transform.Translate(new Vector3(0f, 0f, speed));
+            }
+            else
+            {
+                toDestroy = segment;
+            }
+        }
+
+        if (!(toDestroy == null))
+        {
+            float lastSegmentZpos = segments[nSegments - 1].transform.position.z;
+            segments.Remove(toDestroy);
+
+            float segmentSize = segment.transform.localScale.z;
+            segments.Add(Instantiate(segment, new Vector3(0, 0, lastSegmentZpos + segmentSize * 10), Quaternion.identity));
+
+            Destroy(toDestroy);
+            toDestroy = null;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 100f;
+    public float jumpForce = 4000;
+    Rigidbody rb;
+    bool isGrounded;
+    Vector3 jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+        else
+        {
+            rb.AddForce(transform.up * -20000);
+        }
+
+    }
+}
